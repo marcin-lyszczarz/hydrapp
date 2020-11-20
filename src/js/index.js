a5241479a25eeaaaf0426d0d307008aca675fa2c
Marcin Łyszczarz <lyszczarz.marcin@gmail.com>
2020-11-20 22:01:09 +0100

    added js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,38 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const btnAdd = document.querySelector(".button__add");
+const btnDel = document.querySelector(".button__del");
+
+btnAdd.addEventListener("click", () => {
+  const span = document.querySelector(".hello__glass--number");
+  let number = parseInt(span.textContent);
+
+  console.log(number);
+
+  if (number >= 0) {
+    number++;
+    console.log(number);
+    span.innerHTML = number;
+  }
+});
+btnDel.addEventListener("click", () => {
+  const span = document.querySelector(".hello__glass--number");
+  let number = parseInt(span.textContent);
+
+  console.log(number);
+
+  if (number == 0) {
+      return;
+  }else{
+    number--;
+    console.log(number);
+    span.innerHTML = number;
+  }
+
+});
